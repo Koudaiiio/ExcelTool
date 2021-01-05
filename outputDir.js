500642563a90c5bf725995a77fd27707b93c5687
JDG-020\JDG020 <bytetalking@qq.com>
2021-01-05 20:07:09 +0800

    ts恢复成旧目录， 请暂时不要自定义目录；

--- outputDir.js
+++ outputDir.js
@@ -1,7 +1,7 @@
 module.exports = {
     csv_dir:"g_csv",
     sql_dir:"g_sql",
-    ts_dir:"D://modclient//assets//Configs//ts",
+    ts_dir:"g_ts",
     json_dir:"g_json",
     json_entrypted_dir:"D://modclient//assets//Configs//json_entrypted"
 } 
