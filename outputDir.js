604a3d74adda5c138d2f11a987b0225ed637e942
JDG-020\JDG020 <bytetalking@qq.com>
2021-01-05 18:07:05 +0800

    1

--- outputDir.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    csv_dir:"g_csv",
-    sql_dir:"g_sql",
-    ts_dir:"D://modclient//assets//Configs//ts",
-    json_dir:"g_json",
-    json_entrypted_dir:"D://modclient//assets//Configs//json_entrypted"
-}
